e2a0d521ff3599d5b6d0e01e15bd2429f4647d13
sadiqnabi02 <sadiqun02@gmail.com>
2024-03-28 22:22:40 +0600

    pattern pyramid

--- pattern(pyramid)/pyramid.c
+++ pattern(pyramid)/pyramid.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
 
+int main()
+
+{
+    system("color 72");
+    int n,k,z=1;
+    char ch;
+    scanf("%d %c",&n,&ch);
+    for(int i =1; i <=n ; i++)
+    {
+        for(int j = n-1; j>=i;j--)
+        {
+            printf(" ");
+        }
+        for(k =1 ; k <=z;k++)
+        {
+            printf("%c",ch);
+        }
+        z += 2;
+        printf("\n");
+    }
+}
